92b6bde3d2576c69aafddad3397aca5f8a873526
maciejfedorowiat <maciej.fedorowiat@swmansion.com>
2016-01-15 10:39:20 +0100

    Refactoring

--- projects/ProjectsHandler.py
+++ projects/ProjectsHandler.py
@@ -1,27 +1,34 @@
-import webapp2
 import json
+
+import webapp2
+
 from ProjectConnector import ProjectConnector, Project
-from UsersConnector import UserConnector
+from users.UsersConnector import UserConnector
+
 
 class ProjectsHandler(webapp2.RequestHandler):
 
-    def get(self):
-        sql_conn = ProjectConnector()
-        user_conn = UserConnector()
-        name = str(self.request.get("name"))
-        if name != "":
-            rows = sql_conn.select_all_where("name = '%s'" % name)
-        else:
-            rows = sql_conn.select_all()
-        response = []
+    def __init__(self, request, response):
+        self.initialize(request, response)
+        self.user_conn = UserConnector()
+        self.project_conn = ProjectConnector()
 
+    def get(self):
+        name = str(self.request.get("name"))
+
+        if name != "":
+            rows = self.project_conn.select_all_where("name = '%s'" % name)
+        else:
+            rows = self.project_conn.select_all()
+
+        response = []
         for row in rows:
             obj = {
                 'id': row[0],
                 'name': row[1],
                 'description': row[2],
                 'creatorid': row[3],
-                'creatorname': user_conn.select_where(["name"], "id = %s" % row[3])[0][0],
+                'creatorname': self.user_conn.select_where(["name"], "id = %s" % row[3])[0][0],
                 'money': row[4],
                 'date': str(row[5]),
             }
@@ -30,12 +37,11 @@ class ProjectsHandler(webapp2.RequestHandler):
         self.response.out.write(json.dumps(response))
 
     def post(self):
-        sql_conn = ProjectConnector()
         new_project = Project(str(self.request.get("name")),
                               str(self.request.get("desc")),
                               str(self.request.get("creatorId")))
 
-        sql_conn.insert_into(new_project)
+        self.project_conn.insert_into(new_project)
 
 
 app = webapp2.WSGIApplication([
