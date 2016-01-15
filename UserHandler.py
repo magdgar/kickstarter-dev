45e8098b202edec6991cea91b83ba77d9d0b61bb
JLucka <julia.lucka@me.com>
2016-01-15 14:46:42 +0100

    Add login

---
+++ UserHandler.py
@@ -0,0 +1,25 @@
+from google.appengine.api import users
+import webapp2
+from UsersConnector import UserConnector, User
+
+class UserHandler(webapp2.RequestHandler):
+    def get(self):
+        user = users.get_current_user()
+        if user:
+            user_conn = UserConnector()
+            response = user_conn.select_where(["google_id"], ("google_id = %s" % user.user_id()))
+            if len(response) < 1:
+                new_user = User(str(user.user_id()), user.nickname())
+                user_conn.insert_into(new_user)
+            greeting = ('Welcome, %s! (<a href="%s">sign out</a>)' %
+                        (user.nickname(), users.create_logout_url('/')))
+        else:
+            greeting = ('<a href="%s">Sign in or register</a>.' %
+                        users.create_login_url('/'))
+
+        self.response.out.write(greeting)
+
+
+app = webapp2.WSGIApplication([
+    ('/login', UserHandler),
+])
--- UsersConnector.py
+++ UsersConnector.py
@@ -4,21 +4,23 @@ TABLE_NAME = "users"
 
 
 class User:
-    def __init__(self, name):
+    def __init__(self, google_id, name):
         self.name = name
         self.money = 0
+        self.google_id = google_id
 
     def to_json_obj(self):
         obj = {
-            'id': self.id,
             'name': self.name,
             'money': self.money,
+            'google_id': self.google_id
             }
         return obj
 
     def to_database_query(self):
         name = "'" + self.name + "'"
-        query = {"id": self.id, "name": name, "money": self.money}
+        google_id = "'" + self.google_id + "'"
+        query = {"name": name, "money": self.money, "google_id": google_id}
         return query
 
 
@@ -26,8 +28,10 @@ class UserConnector(SQLConnector):
     def __init__(self):
         SQLConnector.__init__(self)
         self.table_name = TABLE_NAME
+        SQLConnector.table_name = TABLE_NAME
 
-    def insert_into(self, project):
-        return SQLConnector().insert_into(User.to_database_query())
+
+    def insert_into(self, user):
+        return SQLConnector.insert_into(self, user.to_database_query())
 
 
