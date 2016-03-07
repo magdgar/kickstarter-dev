7590d385c10c95cf10a728d921d7d3a0cf3cc321
magdgar <magdgar@gmail.com>
2016-03-07 01:12:22 +0100

    data, time separation

--- backend/projects/ProjectsHandler.py
+++ backend/projects/ProjectsHandler.py
@@ -31,24 +31,23 @@ class ProjectsHandler(webapp2.RequestHandler):
                 'creatorid': row[3],
                 'creatorname': self.user_conn.select_where(["name"], "id = %s" % row[3])[0][0],
                 'money': row[4],
-                'date': str(row[5]),
+                'date': str(row[5]).split(' ')[0],
+                'time': str(row[5]).split(' ')[1]
             }
             response.append(obj)
 
         self.response.out.write(json.dumps(response))
 
     def post(self):
-        #self.response.status = 400
-        #if ';' not in (self.request.get("name") and self.request.get("desc") and self.request.get("creatorId")):
-        new_project = Project(str(self.request.get("name")),
-                              str(self.request.get("desc")),
-                              str(self.request.get("creatorId")))
-        if validate(self.response, new_project):
-            if self.project_conn.insert_into(new_project):
-                self.response.status = 201
-                self.response.write("nice to see your creativity, stay cool")
-            else:
-                self.response.write("no nie")
+        self.response.status = 400
+        if ';' not in (self.request.get("name") and self.request.get("desc") and self.request.get("creatorId")):
+            new_project = Project(str(self.request.get("name")),
+                                str(self.request.get("desc")),
+                                str(self.request.get("creatorId")))
+            if validate(self.response, new_project):
+                if self.project_conn.insert_into(new_project):
+                    self.response.status = 201
+                    self.response.write("nice to see your creativity, stay cool")
 
 app = webapp2.WSGIApplication([
     ('/projects', ProjectsHandler)
