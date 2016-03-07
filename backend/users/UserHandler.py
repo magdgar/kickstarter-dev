0b0408d43c0795734873a13fafa87b16d38bcb32
JLucka <julia.lucka@me.com>
2016-03-07 11:25:23 +0100

    Return single user

--- backend/users/UserHandler.py
+++ backend/users/UserHandler.py
@@ -13,14 +13,12 @@ class UserHandler(webapp2.RequestHandler):
         current_user = users.get_current_user()
         row = self.user_conn.select_all_where("name = '%s'" % current_user)
         print row
-        response = []
         obj = {
             'id': row[0][0],
             'name': row[0][1],
             'money': row[0][2]
             }
-        response.append(obj)
-        return self.response.out.write(json.dumps(response))
+        return self.response.out.write(json.dumps(obj))
 
 
 app = webapp2.WSGIApplication([
